57f5aa9d0a59fc1efd3ef1a081de2c30d6e12dc8
FlowingSPDG <ruk1ium@gmail.com>
2024-09-12 15:55:29 +0000

    プラグインのセットアップ

--- logger.go
+++ logger.go
@@ -1,66 +1,58 @@
 // Package pluginlogger is a demo logger plugin.
 package pluginlogger
 
+// packageのimport
+// ここで標準パッケージ以外を用いる場合、`go mod vendor` が必須となる
+// 基本的には標準パッケージのみで開発することをオススメする
 import (
-	"bytes"
 	"context"
-	"fmt"
+	"io"
 	"net/http"
-	"text/template"
+	"os"
 )
 
-// Config the plugin configuration.
+// Config プラグインの設定が入った構造体
+// yaegiから設定情報が注入されるので、全フィールドをPublicにする
 type Config struct {
-	Headers map[string]string `json:"headers,omitempty"`
+	Prefix string `json:"prefix,omitempty"`
 }
 
-// CreateConfig creates the default plugin configuration.
+// CreateConfig 設定を作成する関数
+// yaegiから呼び出されるため、必須
 func CreateConfig() *Config {
 	return &Config{
-		Headers: make(map[string]string),
+		Prefix: "[YOUR_PLUGIN]",
 	}
 }
 
-// Demo a Demo plugin.
-type Demo struct {
-	next     http.Handler
-	headers  map[string]string
-	name     string
-	template *template.Template
+// Logger プラグインの本体。必要な依存性を内部に保持する
+// http.Handler を実装する
+type Logger struct {
+	// next, name はほぼ必須フィールド
+	next http.Handler
+	name string
+
+	// 設定はそのまま保持する
+	cfg *Config
+
+	// ログの出力先
+	out io.Writer
 }
 
-// New created a new Demo plugin.
+// New プラグイン本体を作成する関数
+// CreateConfig() と同じく、Yaegiから直接呼び出されるので必須
+// また、関数シグネチャは`New(context.Context, http.Handler, *Config, string) (http.Handler, error)` である必要がある
 func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
-	if len(config.Headers) == 0 {
-		return nil, fmt.Errorf("headers cannot be empty")
-	}
-
-	return &Demo{
-		headers:  config.Headers,
-		next:     next,
-		name:     name,
-		template: template.New("demo").Delims("[[", "]]"),
+	return &Logger{
+		next: next,
+		name: name,
+		cfg:  config,
+		out:  os.Stdout,
 	}, nil
 }
 
-func (a *Demo) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
-	for key, value := range a.headers {
-		tmpl, err := a.template.Parse(value)
-		if err != nil {
-			http.Error(rw, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		writer := &bytes.Buffer{}
-
-		err = tmpl.Execute(writer, req)
-		if err != nil {
-			http.Error(rw, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		req.Header.Set(key, writer.String())
-	}
-
-	a.next.ServeHTTP(rw, req)
+// ServeHTTP はミドルウェアの実処理
+func (l *Logger) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	// TODO: 実装
+	l.next.ServeHTTP(rw, req)
 }
--- logger_test.go
+++ logger_test.go
@@ -12,11 +12,6 @@ import (
 
 func TestDemo(t *testing.T) {
 	cfg := pluginlogger.CreateConfig()
-	cfg.Headers["X-Host"] = "[[.Host]]"
-	cfg.Headers["X-Method"] = "[[.Method]]"
-	cfg.Headers["X-URL"] = "[[.URL]]"
-	cfg.Headers["X-URL"] = "[[.URL]]"
-	cfg.Headers["X-Demo"] = "test"
 
 	ctx := context.Background()
 	next := http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {})
@@ -35,16 +30,5 @@ func TestDemo(t *testing.T) {
 
 	handler.ServeHTTP(recorder, req)
 
-	assertHeader(t, req, "X-Host", "localhost")
-	assertHeader(t, req, "X-URL", "http://localhost")
-	assertHeader(t, req, "X-Method", "GET")
-	assertHeader(t, req, "X-Demo", "test")
-}
-
-func assertHeader(t *testing.T, req *http.Request, key, expected string) {
-	t.Helper()
-
-	if req.Header.Get(key) != expected {
-		t.Errorf("invalid header value: %s", req.Header.Get(key))
-	}
+	// TODO: assertion...
 }
