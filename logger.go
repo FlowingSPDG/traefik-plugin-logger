33948b8434d96dc742f14dfdf2c0d07bcb852d70
FlowingSPDG <ruk1ium@gmail.com>
2024-09-12 16:10:39 +0000

    実装

--- logger.go
+++ logger.go
@@ -6,7 +6,7 @@ package pluginlogger
 // 基本的には標準パッケージのみで開発することをオススメする
 import (
 	"context"
-	"io"
+	"log/slog"
 	"net/http"
 	"os"
 )
@@ -14,14 +14,32 @@ import (
 // Config プラグインの設定が入った構造体
 // yaegiから設定情報が注入されるので、全フィールドをPublicにする
 type Config struct {
-	Prefix string `json:"prefix,omitempty"`
+	Prefix   string `json:"prefix,omitempty"`
+	LogLevel string `json:"loglevel,omitempty"`
+}
+
+// logLevel stringのLogLevelをslog.Level に変換する
+func (c *Config) logLevel() slog.Level {
+	switch c.LogLevel {
+	case "debug":
+		return slog.LevelDebug
+	case "info":
+		return slog.LevelInfo
+	case "warn":
+		return slog.LevelDebug
+	case "error":
+		return slog.LevelError
+	default:
+		return slog.LevelInfo
+	}
 }
 
 // CreateConfig 設定を作成する関数
 // yaegiから呼び出されるため、必須
 func CreateConfig() *Config {
 	return &Config{
-		Prefix: "[YOUR_PLUGIN]",
+		Prefix:   "[YOUR_PLUGIN]",
+		LogLevel: "info",
 	}
 }
 
@@ -36,23 +54,44 @@ type Logger struct {
 	cfg *Config
 
 	// ログの出力先
-	out io.Writer
+	logger *slog.Logger
 }
 
 // New プラグイン本体を作成する関数
 // CreateConfig() と同じく、Yaegiから直接呼び出されるので必須
 // また、関数シグネチャは`New(context.Context, http.Handler, *Config, string) (http.Handler, error)` である必要がある
 func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
+	out := os.Stdout
 	return &Logger{
-		next: next,
-		name: name,
-		cfg:  config,
-		out:  os.Stdout,
+		next:   next,
+		name:   name,
+		cfg:    config,
+		logger: slog.New(slog.NewTextHandler(out, nil)),
 	}, nil
+}
+
+// convertRequest *http.Request をslog.Attr に変換するユーティリティ関数
+func convertRequest(req *http.Request) []slog.Attr {
+	return []slog.Attr{
+		slog.String("proto", req.Proto),
+		slog.String("method", req.Method),
+		slog.String("url", req.URL.String()),
+	}
 }
 
 // ServeHTTP はミドルウェアの実処理
 func (l *Logger) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
-	// TODO: 実装
+	// contextを取得
+	ctx := req.Context()
+
+	// fieldに変換
+	field := convertRequest(req)
+
+	// ログを出力
+	l.logger.LogAttrs(ctx, l.cfg.logLevel(), l.cfg.Prefix, field...)
+
+	// next...
 	l.next.ServeHTTP(rw, req)
+
+	// 後処理
 }
